cd5208c5afcf921174b44f891042d1718026ef9c
Asuporte03 <suporte.okdados03@hotmail.com>
2022-11-07 10:56:41 -0300

    Nu try

---
+++ seeker.py
@@ -0,0 +1,20 @@
+#!/bin/env python
+#-*- coding:utf-8 -*-
+import subprocess
+
+
+def limpa():
+    
+    """Just clear hud"""
+    subprocess.call('clear', shell=True)
+    print('Limpei\n')
+    return 0
+
+
+limpa()
+
+
+
+#if __name__ == 'main':
+#    main
+
